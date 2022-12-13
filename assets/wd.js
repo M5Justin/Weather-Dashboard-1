2d44dc3de60a6ada3fe0bef3b397ce4dc3d43d02
justin moran <justinmoranre@gmail.com>
2022-12-13 17:03:37 -0500

    got info from api displayed on page

--- assets/wd.js
+++ assets/wd.js
@@ -1,36 +1,67 @@
 
-var responseText = document.getElementById('response-text');
+var responseText = document.querySelector('#response-text');
+var textSearch = document.querySelector('#textSearch');
+var searchBtn = document.querySelector('#search-button');
 var APIKey = "04d1478f23980571496a41cab11aac4b";
 var city;
 
-var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + "london" + "&appid=" + APIKey;
 
-fetch(requestUrl)
+function fetchResults(place) {
+  var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + APIKey;
+  
+  fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(data);
-    const { temp } = data.main;
-    const place = data.name;
-    const { description, icon } = data.weather[0];
-    const fahrenheit = (temp * 9) / 5 + 32;
-    const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    var { temp } = data.main;
+    var { description, icon } = data.weather[0];
+    var fahrenheit = (temp / 9) / 5 + 32;
+    var place = data.name;
+    console.log(place);
+    console.log(fahrenheit + ' degrees');
+    console.log(description, icon);
+    displayWeather(data);
   })
   .catch(function (error) {
     console.log(error);
   });
+}
+
+function displayWeather(data) {
+  var { temp } = data.main;
+  var { description, icon } = data.weather[0];
+  var fahrenheit = (temp / 9) / 5 + 32;
+  var weather1Div = document.getElementById("weather1");    
+  //creates city heading
+  var heading = document.createElement("h1");
+  heading.innerHTML = data.name;
+  weather1Div.appendChild(heading);
+  //creates temp of city
+  var searchTemp = document.createElement('h2');
+  searchTemp.innerHTML = fahrenheit + ' degrees';
+  weather1Div.appendChild(searchTemp);
+  //creates temp description
+  var cityDesription = document.createElement('h2');
+  cityDesription.innerHTML = description;
+  weather1Div.appendChild(cityDesription);
+}
+
+  var handleSearch = function (event) {
+    event.preventDefault();
+    var search = textSearch.value.trim();
+    fetchResults(search);
+  }
 
   
-  iconImg.src = iconUrl;
-  loc.textContent = `${place}`;
-  desc.textContent = `${description}`;
-  tempC.textContent = `${temp.toFixed(2)} °C`;
-  tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-
   
-  document.querySelector('#txtSearch').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
-      // code for enter
-    }
-});
+  // iconImg.src = iconUrl;
+  // loc.textContent = `${place}`;
+  // desc.textContent = `${description}`;
+  // tempC.textContent = `${temp.toFixed(2)} °C`;
+  // tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+  
+  
+  searchBtn.addEventListener('click', handleSearch);
+  
