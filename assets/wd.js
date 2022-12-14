4a71e99abec326a8a29c05512053bb348d114136
justin moran <justinmoranre@gmail.com>
2022-12-14 16:02:51 -0500

    local storage added

--- assets/wd.js
+++ assets/wd.js
@@ -1,5 +1,5 @@
 
-var responseText = document.querySelector('#response-text');
+
 var textSearch = document.querySelector('#textSearch');
 var searchBtn = document.querySelector('#search-button');
 var APIKey = "04d1478f23980571496a41cab11aac4b";
@@ -8,7 +8,7 @@ var city;
 
 function fetchResults(place) {
   var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + APIKey;
-  
+   
   fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -19,15 +19,14 @@ function fetchResults(place) {
     var { description, icon } = data.weather[0];
     var fahrenheit = (temp / 9) / 5 + 32;
     var place = data.name;
-    console.log(place);
-    console.log(fahrenheit + ' degrees');
-    console.log(description, icon);
+    localStorage.setItem(place, fahrenheit);
     displayWeather(data);
   })
   .catch(function (error) {
     console.log(error);
   });
 }
+
 
 function displayWeather(data) {
   var { temp } = data.main;
@@ -52,16 +51,8 @@ function displayWeather(data) {
     event.preventDefault();
     var search = textSearch.value.trim();
     fetchResults(search);
+    
   }
-
-  
-  
-  // iconImg.src = iconUrl;
-  // loc.textContent = `${place}`;
-  // desc.textContent = `${description}`;
-  // tempC.textContent = `${temp.toFixed(2)} °C`;
-  // tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-  
   
   searchBtn.addEventListener('click', handleSearch);
   
