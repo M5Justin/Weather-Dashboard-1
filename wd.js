a59c23d621043d2639abf10531c9ff56474cb692
unknown <justinmoranre@gmail.com>
2023-02-01 11:58:46 -0500

    fixed issues

--- wd.js
+++ wd.js
@@ -6,53 +6,34 @@ var APIKey = "04d1478f23980571496a41cab11aac4b";
 var city;
 
 
-function fetchResults(place) {
-  var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + place + "&appid=" + APIKey;
-   
-  fetch(requestUrl)
+var getCity = function () {
+  var city = $('#cityInput').val();
+  localStorage.setItem("city", city);
+  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" + APIKey + "&units=imperial";
+  fetch(apiUrl)
   .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    var { temp } = data.main;
-    var { description, icon } = data.weather[0];
-    var fahrenheit = (temp / 9) / 5 + 32;
-    var place = data.name;
-    localStorage.setItem(place, fahrenheit);
-    displayWeather(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
+      if (response.ok) {
+          response.json().then(function (data) {
+              displayCity(data);
+          });
+      } else {
+          alert('Error: ' + response.statusText);
+      }
+      })
+      .catch(function (error) {
+          alert('Unable to connect');
+      });
+};
+
+var displayCity = function (data) {
+  $('#cityTemp').text(data.main.temp);
+  $('#cityWind').text(data.wind.speed);
+  $('#cityHumidity').text(data.main.humidity);
 
 
-function displayWeather(data) {
-  var { temp } = data.main;
-  var { description, icon } = data.weather[0];
-  var fahrenheit = (temp / 9) / 5 + 32;
-  var weather1Div = document.getElementById("weather1");    
-  //creates city heading
-  var heading = document.createElement("h1");
-  heading.innerHTML = data.name;
-  weather1Div.appendChild(heading);
-  //creates temp of city
-  var searchTemp = document.createElement('h2');
-  searchTemp.innerHTML = fahrenheit + ' degrees';
-  weather1Div.appendChild(searchTemp);
-  //creates temp description
-  var cityDesription = document.createElement('h2');
-  cityDesription.innerHTML = description;
-  weather1Div.appendChild(cityDesription);
-}
+};
 
-  var handleSearch = function (event) {
-    event.preventDefault();
-    var search = textSearch.value.trim();
-    fetchResults(search);
-    
-  }
-  
-  searchBtn.addEventListener('click', handleSearch);
-  
+$("#init").click(function(event){
+  event.preventDefault();
+  getCity();
+})
